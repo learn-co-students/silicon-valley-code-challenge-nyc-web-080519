12370dc65a5c6a1f1bb5a3585a9ed7ec60ec8dde
github username <github email address>
2019-08-12 00:13:29 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,35 @@
 class Startup
 
+    attr_reader :name, :founder, :domain
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name =  name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(new_domain, new_name)
+        self.domain = new_domain
+        self.name = new_name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        self.all.find do |startup|
+            startup.founder == founder_name
+        end
+    end
+
+    def self.domains
+        self.all.map do |startup| # select / map? - or either? map can work here, gets all unique and new array 
+            startup.domain.uniq()
+        end
+    end
+    
 end
